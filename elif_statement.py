dd5ac880ee69fd994483c9730a41a39428275529
anji <anji_cruz@msn.com>
2023-12-21 08:27:12 +0800

    elif_statement 12/21

---
+++ elif_statement.py
@@ -0,0 +1,15 @@
+people = 30
+cars = 40
+buses = 15
+
+if cars > people:
+    print ("We should take cars.")
+elif cars < people:
+    print ("We should not take cars.")
+else:
+    print ("We cannot decide.")
+    
+if people > buses:
+    print ("Alright let's just take buses.")
+else:
+    print ("Fine, let us stay home then.")
